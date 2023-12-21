f66ab0db8d9577a8786ab274fe106fd56c7e532b
bharathkkb <bharathkrishnakb@gmail.com>
2023-12-21 21:18:56 +0000

    fix

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -61,10 +61,10 @@ module "managed_instance_group" {
 }
 
 module "load_balancer_default" {
-  name    = "basic-load-balancer-default"
-  source  = "terraform-google-modules/lb/google"
+  source  = "GoogleCloudPlatform/lb/google"
   version = "~> 4.0"
 
+  name         = "basic-load-balancer-default"
   region       = var.region
   service_port = 80
   network      = google_compute_network.network.name
@@ -73,10 +73,10 @@ module "load_balancer_default" {
 }
 
 module "load_balancer_no_hc" {
-  name    = "basic-load-balancer-no-hc"
-  source  = "terraform-google-modules/lb/google"
+  source  = "GoogleCloudPlatform/lb/google"
   version = "~> 4.0"
 
+  name                 = "basic-load-balancer-no-hc"
   region               = var.region
   service_port         = 80
   network              = google_compute_network.network.name
@@ -86,10 +86,10 @@ module "load_balancer_no_hc" {
 }
 
 module "load_balancer_custom_hc" {
-  name    = "basic-load-balancer-custom-hc"
-  source  = "terraform-google-modules/lb/google"
+  source  = "GoogleCloudPlatform/lb/google"
   version = "~> 4.0"
 
+  name         = "basic-load-balancer-custom-hc"
   region       = var.region
   service_port = 8080
   network      = google_compute_network.network.name
